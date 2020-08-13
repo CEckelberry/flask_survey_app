de65259eb034c4f2372f14d97016ea5d80eb01c5
Cole <cole@pop-os.localdomain>
2020-08-13 16:29:19 -0400

    finished implementing sessions and also added css to the survey app

--- app.py
+++ app.py
@@ -1,6 +1,15 @@
-from flask import Flask, request, render_template, redirect, flash
+from flask import (
+    Flask,
+    request,
+    render_template,
+    redirect,
+    flash,
+    session,
+    make_response,
+)
 from random import randint, choice, sample
-from flask_debugtoolbar import DebugToolbarExtension
+
+# from flask_debugtoolbar import DebugToolbarExtension
 from surveys import satisfaction_survey as survey
 
 RESPONSES = []
@@ -8,8 +17,8 @@ RESPONSES = []
 app = Flask(__name__)
 app.config["SECRET_KEY"] = "chickenzarecooll21837"
 
-debug = DebugToolbarExtension(app)
-app.config["DEBUG_TB_INTERCEPT_REDIRECTS"] = False
+# debug = DebugToolbarExtension(app)
+# app.config["DEBUG_TB_INTERCEPT_REDIRECTS"] = False
 
 
 @app.route("/")
@@ -24,24 +33,39 @@ def survey_start():
 
 @app.route("/questions/<int:qid>", methods=["POST", "GET"])
 def show_question(qid):
-    qid = len(RESPONSES)
+    title = survey.title
+    responses = session["responses"]
+    qid = len(responses)
     current_url = request.url[-1::]
     if int(current_url) != qid:
         flash("Please stop trying to fucking access questions out of order, Karen")
         return redirect(f"/questions/{qid}")
-    if qid >= len(survey.questions):
+    elif qid >= len(survey.questions) or current_url == "s":
         return redirect("/thanks")
     question = survey.questions[qid]
     return render_template(
-        "questions.html", question=question, qid=qid, current_url=current_url
+        "questions.html",
+        question=question,
+        qid=qid,
+        current_url=current_url,
+        title=title,
     )
+
+
+@app.route("/sessionview", methods=["POST"])
+def verify_session():
+    session["responses"] = []
+    qid = len(session["responses"])
+    return redirect(f"/questions/{qid}")
 
 
 @app.route("/answer", methods=["POST"])
 def pass_answer():
-    qid = len(RESPONSES)
+    qid = len(session["responses"])
     response = request.form["answer"]
-    RESPONSES.append(response)
+    responses = session["responses"]
+    responses.append(response)
+    session["responses"] = responses
     qid += 1
     return redirect(f"/questions/{qid}")
 
